014087fedea9f912a3a654f19429259f9a8acd1a
Sandy3395 <suman.sandeep1999@gmail.com>
2025-08-19 19:55:57 +0530

    added function

--- src/App.js
+++ src/App.js
@@ -1,41 +1,70 @@
-
-import { useState } from 'react';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import deletebtn from './deletebtn.png'
 
 function App() {
   const [todos, setTodos] = useState([
-     { id: 1, text: "xyz", completed: false},
-     { id: 2, text: "abc", completed: false}
-  ])
-  const [input, setInput] = useState('')
+    { id: 1, text: "xyz", completed: false },
+    { id: 2, text: "abc", completed: false },
+  ]);
+  const [input, setInput] = useState("");
+  const [error, setError] = useState("");
 
-  function addTodo(e){
-         e.preventDefault();
-         setTodos([...todos, {id: Date.now(), text: input, completed: false}])
-         setInput('')
+  function addTodo(e) {
+    e.preventDefault();
+    if (input.trim() === "") {
+      setError("empty msg!!");
+      return;
+    }
+
+    setTodos([...todos, { id: Date.now(), text: input, completed: false }]);
+    setInput("");
+    setError("");
   }
-  // function onSubmithandler(e){
-  //   e.preventDefault();
-  //   addTodo();
-  // }
-  return (
-   <div className='container'>
-     <h2>Todo App</h2>
-     {todos.map((todo) => (
-          <div key={todo.id}>
-           <input type="checkbox" />
-            {todo.text}
-          </div>
-        ))}
 
-     <form onSubmit={addTodo} >
-      <input 
-      value={input}
-      onChange={(e) => setInput(e.target.value)}/>
-      <button >Submit</button>
-     </form>
-   </div>
+  function toggleTodo(id) {
+    setTodos((Todos) =>
+      Todos.map((todo) =>
+        todo.id === id ? { ...todo, completed: !todo.completed } : todo
+      )
+    );
+  }
+
+  function deleteTodo(id) {
+    setTodos((Todos) => Todos.filter((todo) => todo.id !== id));
+  }
+
+  return (
+    <div className="container">
+
+       <form className="form" onSubmit={addTodo}>
+        <input
+          className={`${"input"} ${error ? "input-error" : ""}`}
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+        />
+        <button className="addbtn">Add Task</button>
+      </form>
+      {todos.map((todo) => (
+        <div className="list" key={todo.id}>
+          <input
+            type="checkbox"
+            checked={todo.completed}
+            onChange={() => toggleTodo(todo.id)}
+            className={todo.completed ? "completed" : ""}
+          />
+          <span className={todo.completed ? "completed" : ""}>{todo.text}</span>
+           <button className="btn" onClick={()=> deleteTodo(todo.id)}>
+            <img src={deletebtn} alt="deleteicon"  />
+           </button>
+        </div>
+         
+        
+      ))}
+      
+
+     
+    </div>
   );
 }
 export default App;
-
