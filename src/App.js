fe349956c3bf4916dc942afd4ef29d13ba662e56
Sandeep-suman2469 <sandeepkumarsuman@tothebyte.com>
2025-08-21 19:19:42 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
-import { useState } from "react";
+import { useState , useEffect } from "react";
 import "./App.css";
 import deletebtn from "./deletebtn.png";
 
 function App() {
-  const [todos, setTodos] = useState([
-    { id: 1, text: "xyz", completed: false, deadlineInput: "" },
-    { id: 2, text: "abc", completed: false, deadlineInput: "" },
-  ]);
+
+  const [todos, setTodos] = useState(() => {
+    const storedTodos = localStorage.getItem("todos");
+    return storedTodos ? JSON.parse(storedTodos) : [
+      // { id: 1, text: "xyz", completed: false, date: "2025-08-08", time: "15:15"},
+      // { id: 2, text: "abc", completed: false, date: "", time:"" },
+    ];
+  });
   const [input, setInput] = useState("");
   const [error, setError] = useState("");
   const [time, setTime] = useState("");
   const [date, setDate] = useState("");
+  const [filter, setFilter] = useState("all");
+
+  
+  useEffect(() =>{
+    localStorage.setItem("todos", JSON.stringify(todos))
+  }, [todos])
 
   function addTodo(e) {
     e.preventDefault();
@@ -18,10 +28,9 @@ function App() {
       setError("empty msg!!");
       return;
     }
-
     setTodos([
       ...todos,
-      { id: Date.now(), text: input, completed: false, date, time },
+      { id: Date.now(), text: input, completed: false, date, time }
     ]);
     setInput("");
     setError("");
@@ -47,7 +56,9 @@ function App() {
 
     const diffMs = deadline - now;
     const diffDays = Math.floor(diffMs / (1000 * 60 * 60 * 24));
-    const diffHrs = Math.floor( (diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    const diffHrs = Math.floor(
+      (diffMs % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60)
+    );
 
     if (diffMs < 0) {
       if (diffDays === 0) {
@@ -64,8 +75,22 @@ function App() {
     }
   }
 
+  const getFilteredTodos = () => {
+    if (filter === "completed") {
+      return todos.filter((todo) => todo.completed);
+    } else if (filter === "incomplete") {
+      return todos.filter((todo) => !todo.completed);
+    } else {
+      return todos;
+    }
+  };
+
+  const displayedTodos = getFilteredTodos();
+  const completedTodos = todos.filter((todo) => todo.completed);
+
+  const item_left = todos.length - completedTodos.length;
+
   return (
-    
     <div className="container">
       <form className="form" onSubmit={addTodo}>
         <input
@@ -89,22 +114,59 @@ function App() {
       </form>
 
       <div className="list">
-        {todos.map((todo) => (
+        {displayedTodos.map((todo) => (
           <div className="todo-item" key={todo.id}>
             <input
               type="checkbox"
               checked={todo.completed}
               onChange={() => toggleTodo(todo.id)}
             />
-            <span className={`${"todo-left"} ${todo.completed ? "completed" : ""}`}>
+            <span
+              className={`${"todo-left"} ${todo.completed ? "completed" : ""}`}
+            >
               {todo.text}
             </span>
-            <div className="todo-middle">{deadlineHandler(todo.date, todo.time)}</div>
+            <div className="todo-middle">
+              {deadlineHandler(todo.date, todo.time)}
+            </div>
             <button className="btn" onClick={() => deleteTodo(todo.id)}>
-              <img className="todo-right" src={deletebtn} alt="deleteicon" width="20" height="15" />
+              <img
+                className="todo-right"
+                src={deletebtn}
+                alt="deleteicon"
+                width="20"
+                height="15"
+              />
             </button>
           </div>
         ))}
+      </div>
+
+      <div className="footers">
+        <button onChange={displayedTodos} className="footer">
+          items left {item_left}
+        </button>
+        <button
+          onChange={displayedTodos}
+          className="footer"
+          onClick={() => setFilter("all")}
+        >
+          All
+        </button>
+        <button
+          onChange={displayedTodos}
+          className="footer"
+          onClick={() => setFilter("completed")}
+        >
+          Completed
+        </button>
+        <button
+          onChange={displayedTodos}
+          className="footer"
+          onClick={() => setFilter("incomplete")}
+        >
+          Active
+        </button>
       </div>
     </div>
   );
